fe20c2dd91d2e6bce5944d79c8112eab6a61b596
Pete Bachant <petebachant@gmail.com>
2013-09-18 12:39:00 -0400

    Added more functions, updated examples

--- examples/PyQt/nortekpyqt.py
+++ examples/PyQt/nortekpyqt.py
@@ -11,64 +11,19 @@ from PyQt4 import QtGui
 import PyQt4.Qwt5 as Qwt
 from mainwindow import *
 import sys
-import time
-import os
-import serial
-from serial.tools import list_ports
-
-import win32com.client as wc
-
 import matplotlib.pyplot as plt
 import numpy as np
+from pdcommpy.pdcommpy import PdControl, list_serial_ports
 
-class constants:
-    states = {0 : "Firmware upgrade mode",
-              1 : "Measurement mode",
-              2 : "Command mode",
-              4 : "Data retreival mode",
-              5 : "Confirmation mode"}
-
-
-pdcommx = wc.Dispatch('PdCommATL.PdCommX.1')
-
-
-def safe_str(obj):
-    """ return the byte string representation of obj """
-    try:
-        return str(obj)
-    except UnicodeEncodeError:
-        # obj is unicode
-        return unicode(obj).encode('unicode_escape')
-
-
-def list_serial_ports():
-    # Windows
-    if os.name == 'nt':
-        # Scan for available ports.
-        available = []
-        for i in range(256):
-            try:
-                s = serial.Serial(i)
-                available.append('COM'+str(i + 1))
-                s.close()
-            except serial.SerialException:
-                pass
-        return available
-    else:
-        # Mac / Linux
-        return [port[0] for port in list_ports.comports()]
+pd = PdControl()
 
 # Set up a thread for the connection process
 class ConThread(QThread):
     def __init__(self):
         QThread.__init__(self)
-        finished = QtCore.pyqtSignal()
-        global shit
-        shit = "piss"
-
-        
+        finished = QtCore.pyqtSignal()        
     def run(self):
-        pdcommx.Connect()
+        pd.connect()
         self.isconnected = pdcommx.IsConnected()
         self.finished.emit()
         
@@ -116,28 +71,24 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.comport_combobox.addItems(QStringList(self.comports))
         
         # Set some stuff up
-        pdcommx.StartOnSynch = 0
-        pdcommx.SynchMaster = 1
-        pdcommx.CoordinateSystem = 1
-        pdcommx.DiagnosticsMode = 0
-
+        pd.set_start_on_synch(True)
+        pd.set_synch_master(True)
+        pd.set_coordinate_system("XYZ")
         
     def on_connect_click(self):
         comport = self.ui.comport_combobox.currentText()
-        pdcommx.SerialPort = str(comport)
+        pd.set_serial_port(str(comport))
         self.clabel.setText("Attempting to connect via " + comport + "... ")
         self.conthread = ConThread()
         self.conthread.finished.connect(self.on_con_finish)
         self.conthread.start()
         
-    
     def on_mpl(self):
         plt.figure()
         x = np.linspace(0, 2*np.pi, 100)
         y = np.sin(x)
         plt.plot(x, y)
         plt.show()
-        
         
     def on_con_finish(self):
         
@@ -151,26 +102,22 @@ class MainWindow(QtGui.QMainWindow):
             self.isconnected = False
         
         self.clabel.setText(text)
-        
         self.ui.start_button.setEnabled(self.isconnected)
         self.ui.stop_button.setEnabled(self.isconnected)
         self.ui.getconfig_button.setEnabled(self.isconnected)
         
-        
     def on_start(self):
         if self.isconnected == True:
-            pdcommx.Stop()
-            pdcommx.SetConfig()
-            pdcommx.StartDiskRecording("test", False)
-            pdcommx.Start(False)
-        
+            pd.Stop()
+            pd.set_config()
+            pd.start_disk_recording("test")
+            pd.start()
         
     def on_stop(self):
         if self.isconnected == True:
-            pdcommx.Stop()
-            pdcommx.StopDiskRecording()
+            pd.stop()
+            pd.stop_disk_recording()
 
-    
     def create_plot(self):
         self.plot = self.ui.plot
         self.plot.setCanvasBackground(Qt.white)
@@ -184,26 +131,21 @@ class MainWindow(QtGui.QMainWindow):
         self.curve.setPen(self.pen)
         self.curve.attach(self.plot)
         
-    
     def on_getconfig(self):
-        config = pdcommx.GetHeadConf()
-        print config.encode('utf-8', 'replace')
+        config = pd.get_prod_conf()
+        print config
         
     def on_timer(self):
-        st = pdcommx.InquireState()
-        self.st_label.setText(constants.states[st] + " ")
-        
-        vel = pdcommx.GetVarVelocity()    
-        
+        st = pd.inquire_state()
+        self.st_label.setText(st + " ")
+        vel = pd.get_var_velocity() 
         self.vel_label.setText("Velocity: " + str(vel[0]) + " ")
 
-        
     def closeEvent(self, event):
         if self.isconnected == True:
-            pdcommx.Stop()
-            pdcommx.Disconnect()
-            pdcommx.StopDiskRecording()
-            
+            pd.stop()
+            pd.disconnect()
+            pd.stop_disk_recording()
         plt.close('all')
         
 
--- pdcommpy.py
+++ pdcommpy.py
@@ -7,39 +7,6 @@ Created on Mon Aug 12 17:16:04 2013
 import win32com.client as _wc
 import numpy as np
 
-#class PdEvents(object):
-#    def __init__(self):
-#        self.data = {"u" : np.array([]),
-#                     "v" : np.array([]),
-#                     "w" : np.array([]),
-#                     "w2" : np.array([]),
-#                     "snr" : np.array([])}
-#    def append_data(self):
-#        self.data["u"] = np.append(self.data["u"], self.u)
-#        self.data["v"] = np.append(self.data["v"], self.v)
-#        self.data["w"] = np.append(self.data["w"], self.w)
-#        self.data["w2"] = np.append(self.data["w2"], self.w2)
-#        self.data["snr"] = np.append(self.data["snr"], self.snr)
-#    def OnNewData(self, hType=1):
-#        print "New data"
-#        self.snr = get_snr(1)
-#        self.u = get_vel(1,1)
-#        self.v = get_vel(1,2)
-#        self.w = get_vel(1,3)
-#        self.w2 = get_vel(1,4)
-#        self.snr_u = self.snr[0]
-#        self.snr_v = self.snr[1]
-#        self.snr_w = self.snr[3]
-#        self.snr_w2 = self.snr[4]
-#        self.corr_u = get_corr(1,1)
-#        self.corr_v = get_corr(1,2)
-#        self.corr_w = get_corr(1,3)
-#        self.corr_w2 = get_corr(1,4)
-#        self.append_data()
-
-#_pdcommx = _wc.DispatchWithEvents('PdCommATL.PdCommX.1', PdEvents)
-_pdcommx = _wc.Dispatch('PdCommATL.PdCommX.1')
-
 # Device states dict
 states = {0 : "Firmware upgrade mode",
           1 : "Measurement mode",
@@ -56,19 +23,22 @@ instruments = {0 : "Aquadopp",
                4 : "EasyQ",
                5 : "Continental",
                6 : "Vectrino"}
-               
-# Functions
-def connect():
-    """Connects to the instrument and checks its status."""
-    _pdcommx.Connect()
     
-def set_serial_port(port):
-    """Sets serial port (input as a string)."""
-    _pdcommx.SerialPort = port
+# Dictionary of velocity ranges (in m/s)
+velranges = {"Vector" : {0 : 7.0,
+                         1 : 4.0,
+                         2 : 2.0,
+                         3 : 1.0,
+                         4 : 0.3,
+                         5 : 0.1,
+                         6 : 0.01},
+             "Vectrino" : {0 : 4.0,
+                           1 : 2.5,
+                           2 : 1.0,
+                           3 : 0.3,
+                           4 : 0.1,
+                           5 : 0.03}}
 
-def get_serial_port():
-    """Get current serial port."""
-    return str(_pdcommx.SerialPort)
 
 def list_serial_ports():
     """List serial ports on current machine."""
@@ -89,174 +59,48 @@ def list_serial_ports():
         return available
     else:
         # Mac / Linux
-        return [port[0] for port in list_ports.comports()]
-
-def get_firmware_version():
-    """Returns the firmware version. Must be called after connect(), which 
-    reads the hardware and software configuration from the instrument."""
-    return _pdcommx.GetFirmwareVersion()
-
-def get_head_serialno():
-    """Returns the head serial number."""
-    return _pdcommx.GetHeadSerialNo()
-
-def set_start_on_synch(choice):
-    _pdcommx.StartOnSynch = int(choice)
-    
-def set_sample_on_synch(choice):
-    """Sets sample on synch parameter, meaning device will sample at each
-    pulse received."""
-    _pdcommx.SampleOnSynch = int(choice)
-
-def set_synch_master(choice):
-    """Set the device as a synch master."""
-    _pdcommx.SynchMaster = int(choice)
-
-def set_sampling_rate(rate):
-    """Set sample rate in Hz"""
-    _pdcommx.SamplingRate = rate
-
-def is_connected():
-    """Returns a bool indicating connection status."""
-    return bool(_pdcommx.IsConnected())
-
-def set_vel_range(velrange):
-    """Sets instrument velocity range."""
-    _pdcommx.VelRange = int(velrange)    
-
-def set_config():
-    """Writes the configuration (properties) to the instrument.
-    Note that for most of the property settings to take effect, the SETCONFIG
-    method must be called prior to Start command. See also CONNECT."""
-    _pdcommx.SetConfig()    
-
-def start(brecorder=False):
-    """Starts online measurements based on the current configuration
-    of the instrument. If :code:`brecorder = True` data is stored to 
-    a new file in the recorder."""
-    _pdcommx.Start(brecorder)
-    
-def start_disk_recording(filename, autoname=False):
-    """Starts data recording to disk. Specify the filename without extension. 
-    If autoname = True a new file will be opened for data recording each time 
-    the specified time interval has elapsed. The current date and time is then 
-    automatically added to the filename."""
-    _pdcommx.StartDiskRecording(filename, autoname)
-
-def stop_disk_recording():
-    """Stops data recording to disk."""
-    _pdcommx.StopDiskRecording()
-
-def disconnect():
-    """Disconnects device."""
-    _pdcommx.Disconnect()
-
-def stop():
-    """Stops data collection."""
-    _pdcommx.Stop()    
-
-def get_prod_conf():
-    """Returns the hardware configuration structure as a VARIANT array. 
-    See the Paradopp system integrators manual for a description of the 
-    binary data structures."""
-    return _pdcommx.GetProdConf()
-
-def get_vel(cell, beam):
-    """Gets the most recent velocity data for the specified cell and beam
-    (m/s). Cell and beam numbering start at 1."""
-    return _pdcommx.GetVel(cell, beam)
-
-def get_velocity(cell):
-    """Gets the most recent velocity data for the specified cell number.
-    Cell numbering starts at 1."""
-    return _pdcommx.GetVelocity(cell)
-    
-def get_var_velocity():
-    """Gets the most recent velocity data as a variant type array."""
-    return _pdcommx.GetVarVelocity
-    
-def get_snr(cell):
-    """Gets the most recent SNR data for the specified cell number."""
-    return _pdcommx.GetSNR(cell)
-    
-def get_sampling_volume_value(nSVIndex, nTLIndex):
-    """Returns the Sampling Volume corresponding to the Sampling Volume and 
-    Transmit Length indices."""
-    return _pdcommx.GetSamplingVolumeValue(nSVIndex, nTLIndex)
-
-def get_clock():
-    """Returns the current date and time of the RTC in the instrument."""
-    return _pdcommx.GetClock()
-       
-def get_corr(cell, beam):
-    """Returns the most recent correlation data for the specified cell 
-    and beam number. Cell and beam numbering start at 1."""
-    return _pdcommx.GetCorr(cell, beam)
-    
-def get_instrument(as_string=True):
-    """Returns the instrument type."""
-    if as_string:
-        return instruments[_pdcommx.GetInstrument()]
-    else:
-        return _pdcommx.GetInstrument()
-
-def get_data_block(hType=1):
-    """Returns the most recent measurement data structure as a VARIANT array. 
-    See the Paradopp system integrators manual for a description of the 
-    binary data structures."""
-    return _pdcommx.GetDataBlock(hType)
-    
-def get_vertical_vel_prec():
-    """Returns the vertical velocity precision."""
-    return _pdcommx.GetVerticalVelPrec()
-
-def get_horizontal_vel_prec():
-    """Returns the horizontal velocity precision."""
-    return _pdcommx.GetHorizontalVelPrec()
-    
-def get_error_message():
-    """Gets the most recent error message."""
-    return _pdcommx.GetErrorMessage()
-
-def start_distance_check():
-    """Starts distance measurements."""
-    _pdcommx.StartDistanceCheck()
-
-def validate_config():
-    """Returns True if the instrument configuration is valid."""
-    return bool(_pdcommx.ValidateConfig())
-
-def inquire_state(as_string=True):
-    """Returns the device state. If as_string is false, returns an int."""
-    if as_string:
-        return states[_pdcommx.InquireState()]
-    else:
-        return _pdcommx.InquireState()
+        return [port[0] for port in list_ports.comports()]           
 
 
 class PdControl(object):
-    """A PdComm control object."""
+    """A PdComm control object. When data is being acquired Numpy arrays
+    are being created in memory."""
     def __init__(self):
         self.pdx = _wc.DispatchWithEvents('PdCommATL.PdCommX.1', self.PdEvents)
+        
+        # Set data associated with callback functions
         self.data = self.pdx.data
+        self.u = self.pdx.u
+        self.v = self.pdx.v
+        
+        # Set connected and state variables
         self.is_connected()
         self.inquire_state()
+        
+        self.sample_rate = self.pdx.SamplingRate
 
     class PdEvents(object):
+        """OnNewData event handler."""
         def __init__(self):
-            self.data = {"u" : np.array([]),
+            self.data = {"t" : np.array([]),
+                         "u" : np.array([]),
                          "v" : np.array([]),
                          "w" : np.array([]),
                          "w2" : np.array([]),
                          "snr_u" : np.array([])}
+            self.u = 0.0
+            self.v = 0.0
+            self.w = 0.0
         def append_data(self):
             self.data["u"] = np.append(self.data["u"], self.u)
             self.data["v"] = np.append(self.data["v"], self.v)
             self.data["w"] = np.append(self.data["w"], self.w)
+            self.data["corr_u"] = np.append(self.data["corr_u"], self.corr_u)
+            self.data["corr_v"] = np.append(self.data["corr_v"], self.corr_v)
 #            self.data["w2"] = np.append(self.data["w2"], self.w2)
 #            self.data["snr_u"] = np.append(self.data["snr_u"], self.snr_u)
+            self.data["t"] = np.arange(len(self.data["u"]))/self.SamplingRate
         def OnNewData(self, hType=1):
-#            print "New data"
 #            self.snr = self.GetSNR(1)
             self.u = self.GetVel(1,1)
             self.v = self.GetVel(1,2)
@@ -284,27 +128,6 @@ class PdControl(object):
         """Get current serial port."""
         return str(self.pdx.SerialPort)
     
-    def list_serial_ports(self):
-        """List serial ports on current machine."""
-        import os
-        import serial
-        from serial.tools import list_ports
-        # Windows
-        if os.name == 'nt':
-            # Scan for available ports.
-            available = []
-            for i in range(256):
-                try:
-                    s = serial.Serial(i)
-                    available.append('COM'+str(i + 1))
-                    s.close()
-                except serial.SerialException:
-                    pass
-            return available
-        else:
-            # Mac / Linux
-            return [port[0] for port in list_ports.comports()]
-    
     def get_firmware_version(self):
         """Returns the firmware version. Must be called after connect(), which 
         reads the hardware and software configuration from the instrument."""
@@ -326,9 +149,10 @@ class PdControl(object):
         """Set the device as a synch master."""
         self.pdx.SynchMaster = int(choice)
     
-    def set_sampling_rate(self, rate):
+    def set_sample_rate(self, rate):
         """Set sample rate in Hz"""
         self.pdx.SamplingRate = rate
+        self.sample_rate = self.pdx.SamplingRate
     
     def is_connected(self):
         """Returns a bool indicating connection status."""
@@ -337,7 +161,19 @@ class PdControl(object):
     
     def set_vel_range(self, velrange):
         """Sets instrument velocity range."""
-        self.pdx.VelRange = int(velrange)    
+        self.pdx.VelRange = int(velrange)
+        
+    def set_coordinate_system(self, coordsys):
+        """Sets instrument coordinate system. Accepts an int or string."""
+        if coordsys == "ENU":
+            ncs = 0
+        elif coordsys == "XYZ":
+            ncs = 1
+        elif coordsys == "Beam":
+            ncs = 2
+        else:
+            nsc = coordsys
+        self.pdx.CoordinateSystem = ncs
     
     def set_config(self):
         """Writes the configuration (properties) to the instrument.
@@ -376,6 +212,10 @@ class PdControl(object):
         binary data structures."""
         return self.pdx.GetProdConf()
     
+    def get_start_on_synch(self):
+        """Returns start on synch option value."""
+        return bool(self.pdx.StartOnSynch)
+    
     def get_vel(self, cell, beam):
         """Gets the most recent velocity data for the specified cell and beam
         (m/s). Cell and beam numbering start at 1."""
@@ -390,6 +230,10 @@ class PdControl(object):
         """Gets the most recent velocity data as a variant type array."""
         return self.pdx.GetVarVelocity
         
+    def get_vel_range(self):
+        """Returns instrument velocity range."""
+        return self.pdx.VelRange
+        
     def get_snr(self, cell, other):
         """Gets the most recent SNR data for the specified cell number."""
         return self.pdx.GetSNR(cell, other)
@@ -398,6 +242,10 @@ class PdControl(object):
         """Returns the Sampling Volume corresponding to the Sampling Volume and 
         Transmit Length indices."""
         return self.pdx.GetSamplingVolumeValue(nSVIndex, nTLIndex)
+        
+    def get_sampling_volume(self):
+        """Returns the device samping volume."""
+        return self.pdx.GetSamplingVolume()
     
     def get_clock(self):
         """Returns the current date and time of the RTC in the instrument."""
@@ -429,6 +277,16 @@ class PdControl(object):
         """Returns the horizontal velocity precision."""
         return self.pdx.GetHorizontalVelPrec()
         
+    def get_coordinate_system(self):
+        """Returns the device coordinate system."""
+        csys = self.pdx.CoordinateSystem
+        if csys == 0:
+            return "ENU"
+        elif csys == 1:
+            return "XYZ"
+        elif csys == 2:
+            return "Beam"
+        
     def get_error_message(self):
         """Gets the most recent error message."""
         return self.pdx.GetErrorMessage()
@@ -452,4 +310,7 @@ class PdControl(object):
 
 if __name__ == "__main__":
     pdcontrol = PdControl()
+    pdcontrol.set_serial_port("COM2")
     print pdcontrol.inquire_state()
+    pdcontrol.set_sample_rate(200)
+    print pdcontrol.get_coordinate_system()
